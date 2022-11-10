5c13fdee3ecdf19f3a79bb3db6a65fffcab973fb
cbolles <collinbolles@gmail.com>
2022-11-10 11:40:52 -0500

    Fix linting issues

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,21 +1,17 @@
-import { DiskHealthIndicator, HealthCheck, HealthCheckService, MemoryHealthIndicator } from '@nestjs/terminus';
+import { DiskHealthIndicator, HealthCheck, HealthCheckResult, HealthCheckService, HealthIndicatorResult, MemoryHealthIndicator } from '@nestjs/terminus';
 import { Controller, Get } from '@nestjs/common';
-
 
 @Controller('health')
 export class HealthController {
-  constructor(
-    private readonly health: HealthCheckService,
-    private readonly disk: DiskHealthIndicator,
-    private readonly memory: MemoryHealthIndicator) {}
+  constructor(private readonly health: HealthCheckService, private readonly disk: DiskHealthIndicator, private readonly memory: MemoryHealthIndicator) {}
 
   @Get()
   @HealthCheck()
-  healthCheck() {
+  healthCheck(): Promise<HealthCheckResult> {
     // TODO: Once configuration is added, remove hard coded limits
     return this.health.check([
-      () => this.disk.checkStorage('storage', { thresholdPercent: 0.5, path: '/' }),
-      () => this.memory.checkHeap('memory_heap', 100 * 1024 * 1024),
+      (): PromiseLike<HealthIndicatorResult> => this.disk.checkStorage('storage', { thresholdPercent: 0.5, path: '/' }),
+      (): PromiseLike<HealthIndicatorResult> => this.memory.checkHeap('memory_heap', 100 * 1024 * 1024)
     ]);
   }
 }
