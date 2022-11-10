57699185f16f2af1b8b49d165ddf39a453dcd762
cbolles <collinbolles@gmail.com>
2022-11-10 14:41:31 -0500

    Add GraphQL to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,19 @@ import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import { ConfigModule } from '@nestjs/config';
 import config from './config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   imports: [
     HealthModule,
     ConfigModule.forRoot({
       load: [config]
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     })
   ],
   controllers: [AppController],
