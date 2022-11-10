2abbea239ae7e69381f545ead3fcac8bc9aa6553
cbolles <collinbolles@gmail.com>
2022-11-10 13:55:41 -0500

    Add in config module

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,17 +1,17 @@
 import { DiskHealthIndicator, HealthCheck, HealthCheckResult, HealthCheckService, HealthIndicatorResult, MemoryHealthIndicator } from '@nestjs/terminus';
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('health')
 export class HealthController {
-  constructor(private readonly health: HealthCheckService, private readonly disk: DiskHealthIndicator, private readonly memory: MemoryHealthIndicator) {}
+  constructor(private readonly health: HealthCheckService, private readonly disk: DiskHealthIndicator, private readonly memory: MemoryHealthIndicator, private readonly config: ConfigService) {}
 
   @Get()
   @HealthCheck()
   healthCheck(): Promise<HealthCheckResult> {
-    // TODO: Once configuration is added, remove hard coded limits
     return this.health.check([
-      (): PromiseLike<HealthIndicatorResult> => this.disk.checkStorage('storage', { thresholdPercent: 0.5, path: '/' }),
-      (): PromiseLike<HealthIndicatorResult> => this.memory.checkHeap('memory_heap', 100 * 1024 * 1024)
+      (): PromiseLike<HealthIndicatorResult> => this.disk.checkStorage('storage', { thresholdPercent: this.config.getOrThrow('health.storageThreshold'), path: '/' }),
+      (): PromiseLike<HealthIndicatorResult> => this.memory.checkHeap('memory_heap', this.config.getOrThrow('health.memoryThreshold'))
     ]);
   }
 }
