bd5f1420bfd6e361c7a2248402b6731e8755d3e4
cbolles <collinbolles@gmail.com>
2022-11-28 13:46:39 -0500

    Write script to load services into database

---
+++ scripts/src/commands/load/services.ts
@@ -0,0 +1,84 @@
+import { Command, Flags } from '@oclif/core';
+import { MongoClient, ObjectId } from 'mongodb';
+import { services } from '../../assets/services.json';
+
+/**
+ * Script to load in services from a JSON file
+ */
+export default class LoadServices extends Command {
+  static description = 'Load services from a JSON file';
+
+  static flags = {
+    file: Flags.string({
+      char: 'f',
+      description: 'JSON file to load',
+      required: false,
+      default: 'src/assets/services.json'
+    }),
+    db: Flags.string({
+      char: 'd',
+      description: 'database to load into',
+      default: 'mongodb://localhost:27017/damplab',
+      required: false,
+    })
+  }
+
+  client: MongoClient | null = null;
+
+  async run() {
+    const { flags } = await this.parse(LoadServices);
+
+    // Connect to the database
+    this.client = await MongoClient.connect(flags.db);
+    await this.client.connect();
+
+    // Insert the services into the database
+    await this.insertIntoDatabase(services);
+
+    // Close the connection
+    await this.client.close();
+  }
+
+  /**
+   * Converts the services from the JSON format into a format that can be
+   * inserted into the database. This handles the logic of connecting
+   * the "allowedConnections" up
+   */
+  async insertIntoDatabase(services: any[]) {
+    /** Maps the ID as defined in the JSON file to the ID in the database */
+    const serviceMap = new Map<string, ObjectId>();
+
+    // For each of the services, save the info not including
+    // the allowed connections
+    for (const service of services) {
+      // Get a copy of the data without the allowed connections
+      const copy = { ...service };
+      delete copy.allowedConnections;
+
+      // Insert the service into the database
+      const result = await this.client!.db().collection('services').insertOne(copy);
+
+      // Update the map
+      serviceMap.set(service.id, result.insertedId);
+    }
+
+    // For each of the services, update the allowed connections
+    for (const service of services) {
+      // Get the ID of the service
+      const id = serviceMap.get(service.id);
+
+      // Get the IDs of the allowed connections
+      const allowedConnections = service.allowedConnections.map((id: string) => {
+        return serviceMap.get(id)
+      });
+
+      // Update the service
+      await this.client!.db().collection('services').updateOne({
+        _id: id
+      }, {
+        $set: { allowedConnections: allowedConnections }
+      });
+    }
+  }
+
+}
---
+++ scripts/src/commands/reset/index.ts
@@ -0,0 +1,29 @@
+import { Command, Flags } from '@oclif/core';
+import { MongoClient } from 'mongodb';
+
+export default class Reset extends Command {
+  static description = 'Clear out all data in the database';
+
+  static flags = {
+    db: Flags.string({
+      char: 'd',
+      description: 'database to reset',
+      default: 'mongodb://localhost:27017/damplab',
+      required: false
+    })
+  }
+
+  async run() {
+    const { flags } = await this.parse(Reset);
+
+    // Connect to the database
+    const client = await MongoClient.connect(flags.db);
+    await client.connect();
+
+    // Drop the database
+    await client.db().dropDatabase();
+    this.log(`Database ${flags.db} dropped`);
+
+    await client.close();
+  }
+}
