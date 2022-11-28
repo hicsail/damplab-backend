76a89a706e5c13e5dca51558cbd65b1619e184f0
cbolles <collinbolles@gmail.com>
2022-11-28 11:06:21 -0500

    Have the ability to load config either from just environment variables or config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,9 +14,7 @@ import { DampLabServicesModule } from './services/damplab-services.module';
 @Module({
   imports: [
     HealthModule,
-    ConfigModule.forRoot({
-      load: [config]
-    }),
+    getConfigModule(),
     OrdersModule,
     DampLabServicesModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
@@ -31,3 +29,33 @@ import { DampLabServicesModule } from './services/damplab-services.module';
   providers: [AppService]
 })
 export class AppModule {}
+
+
+/**
+ * Get the config module based on the specific environment file to load from.
+ *
+ * This function checkfor the existence of the environemnt variable
+ * `ENV_FILE`. If it exists, the function will load the config from that file,
+ * if that file does not exist, only use existing environment variables.
+ * For example, if `ENV_FILE=dev` then the file `.env.dev` will be loaded.
+ *
+ * Part of the benefit of this approach is that tools that allow you to
+ * enter environment variables are easily supported such as docker-compose.
+ * In such cases, by not providing an `ENV_FILE` environment variable, the
+ * config will be loaded from the environment variables.
+ */
+function getConfigModule() {
+  // If the `ENV_FILE` is provided, load from that file
+  if (process.env.ENV_FILE) {
+    return ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.ENV_FILE}`,
+      load: [config],
+    });
+  }
+
+  // Otherwise don't load from a file, just use the environment variables
+  return ConfigModule.forRoot({
+    ignoreEnvFile: true,
+    load: [config],
+  });
+}
