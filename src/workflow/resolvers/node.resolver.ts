8f36bd644b1bb6f7647481a263bf45a7a2c339e0
cbolles <collinbolles@gmail.com>
2023-01-24 11:18:07 -0500

    Fix node resolver for service

--- src/workflow/resolvers/node.resolver.ts
+++ src/workflow/resolvers/node.resolver.ts
@@ -2,6 +2,7 @@ import { WorkflowNode } from '../models/node.model';
 import { Parent, Resolver, ResolveField } from '@nestjs/graphql';
 import { DampLabServices } from '../../services/damplab-services.services';
 import { DampLabService } from '../../services/models/damplab-service.model';
+import mongoose from 'mongoose';
 
 @Resolver(() => WorkflowNode)
 export class WorkflowNodeResolver {
@@ -9,8 +10,8 @@ export class WorkflowNodeResolver {
 
   @ResolveField()
   async service(@Parent() node: WorkflowNode): Promise<DampLabService> {
-    if (typeof node.service === 'string') {
-      const service = await this.damplabServices.findOne(node.service);
+    if (node.service instanceof mongoose.Types.ObjectId) {
+      const service = await this.damplabServices.findOne(node.service.toString());
       if (service !== null) {
         return service;
       } else {
