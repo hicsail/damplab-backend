f398f0756e16c2d12b4a63c58a10a7982115bf63
cbolles <collinbolles@gmail.com>
2023-01-25 15:46:58 -0500

    Remove name query from Workflow

--- src/workflow/workflow.service.ts
+++ src/workflow/workflow.service.ts
@@ -16,14 +16,6 @@ export class WorkflowService {
   ) {}
 
   async create(createWorkflowInput: AddWorkflowInput): Promise<Workflow> {
-    // For now, if a workflow with the same name exists, we will just
-    // remove the existing workflow and replace it with the new one.
-    // TODO: Once the auth system is integrated, add in concept of a user
-    const existingWorkflow = await this.findByName(createWorkflowInput.name);
-    if (existingWorkflow) {
-      await this.remove(existingWorkflow);
-    }
-
     // Make the nodes
     const nodes = await Promise.all(createWorkflowInput.nodes.map((node) => this.nodeService.create(node)));
 
@@ -38,17 +30,7 @@ export class WorkflowService {
 
     const workflow = { ...createWorkflowInput, nodes, edges, state: WorkflowState.SUBMITTED };
 
-    if (existingWorkflow) {
-      await this.workflowModel.updateOne({ _id: existingWorkflow._id }, workflow).exec();
-      const result = await this.workflowModel.findOne({ _id: existingWorkflow._id });
-      return result!;
-    }
-
     return this.workflowModel.create(workflow);
-  }
-
-  async findByName(name: string): Promise<Workflow | null> {
-    return this.workflowModel.findOne({ name });
   }
 
   async findById(id: string): Promise<Workflow | null> {
@@ -67,15 +49,5 @@ export class WorkflowService {
 
   async getByState(state: WorkflowState): Promise<Workflow[]> {
     return this.workflowModel.find({ state });
-  }
-
-  private async remove(workflow: Workflow): Promise<void> {
-    // Remove all nodes
-    const workflowNodes = workflow.nodes.map((node) => node._id.toString());
-    await this.nodeService.removeByIDs(workflowNodes);
-
-    // Remove all edges
-    const workflowEdges = workflow.edges.map((edge) => edge._id.toString());
-    await this.edgeService.removeByIDs(workflowEdges);
   }
 }
