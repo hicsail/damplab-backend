59ba76c03e9784810d95d6d929031eb06af6e377
Collin Bolles <collinbolles@gmail.com>
2023-02-10 09:47:03 -0500

    feature: Job Creates Workflows
    * Have workflow creation take place on Job creation

--- src/job/job.dto.ts
+++ src/job/job.dto.ts
@@ -1,26 +1,9 @@
-import { Injectable, PipeTransform } from '@nestjs/common';
-import { ID, InputType, OmitType, Field } from '@nestjs/graphql';
-import { Workflow } from '../workflow/models/workflow.model';
-import { WorkflowPipe } from '../workflow/workflow.pipe';
+import { InputType, OmitType, Field } from '@nestjs/graphql';
 import { Job } from './job.model';
-import mongoose from 'mongoose';
+import { AddWorkflowInput } from '../workflow/dtos/add-workflow.input';
 
 @InputType()
 export class CreateJob extends OmitType(Job, ['_id', 'workflows', 'submitted'] as const, InputType) {
-  @Field(() => [ID], { description: 'The workflows that were submitted together' })
-  workflows: string[];
-}
-
-export type CreateJobFull = Omit<Job, '_id' | 'submitted'>;
-
-/** Transform the list of IDs into workflows, ensures the workflows exist */
-@Injectable()
-export class CreateJobPipe implements PipeTransform<CreateJob, Promise<CreateJobFull>> {
-  constructor(private readonly workflowPipe: WorkflowPipe) {}
-
-  async transform(value: CreateJob): Promise<CreateJobFull> {
-    const workflows: Workflow[] = await Promise.all(value.workflows.map((id) => this.workflowPipe.transform(id)));
-    const workflowIds: mongoose.Types.ObjectId[] = workflows.map((workflow) => new mongoose.Types.ObjectId(workflow._id));
-    return { ...value, workflows: workflowIds };
-  }
+  @Field(() => [AddWorkflowInput], { description: 'The workflows that were submitted together' })
+  workflows: AddWorkflowInput[];
 }
--- src/job/job.service.ts
+++ src/job/job.service.ts
@@ -2,15 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Job, JobDocument } from './job.model';
 import { Model } from 'mongoose';
-import { CreateJobFull } from './job.dto';
+import { CreateJob } from './job.dto';
 import { Workflow } from '../workflow/models/workflow.model';
+import { WorkflowService } from '../workflow/workflow.service';
 
 @Injectable()
 export class JobService {
-  constructor(@InjectModel(Job.name) private readonly jobModel: Model<JobDocument>) {}
+  constructor(@InjectModel(Job.name) private readonly jobModel: Model<JobDocument>, private readonly workflowService: WorkflowService) {}
 
-  async create(createJobInput: CreateJobFull): Promise<Job> {
-    return this.jobModel.create(createJobInput);
+  async create(createJobInput: CreateJob): Promise<Job> {
+    const workflowIDs = await Promise.all(
+      createJobInput.workflows.map(async (workflow) => {
+        const createdWorkflow = await this.workflowService.create(workflow);
+        return createdWorkflow._id;
+      })
+    );
+    return this.jobModel.create({ ...createJobInput, workflows: workflowIDs });
   }
 
   async findById(id: string): Promise<Job | null> {
