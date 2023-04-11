60da9fd217046cc69e15b9162742b5c2517b3354
cbolles <collinbolles@gmail.com>
2023-04-11 10:13:58 -0400

    Add ability to create projects

--- scripts/elab-exploration/src/commands/project/create.ts
+++ scripts/elab-exploration/src/commands/project/create.ts
@@ -1,7 +1,64 @@
-import { Command } from '@oclif/core';
+import { Command, Flags } from '@oclif/core';
 
 export default class CreateProject extends Command {
-  async run(): Promise<void> {
+  static flags = {
+    name: Flags.string({
+      description: 'Name of the project',
+      required: false,
+    }),
+    longname: Flags.string({
+      description: 'Longer name of the project',
+      required: false
+    }),
+    notes: Flags.string({
+      description: 'Additional notes that may need to be provided',
+      required: false
+    }),
+    label: Flags.string({
+      description: 'Comma seperated list of labels',
+      required: false
+    }),
+    projectMeta: Flags.string({
+      description: 'JSON array with the string fields "name", "value", "metaType"',
+      required: false
+    })
+  };
 
+  async run(): Promise<void> {
+    // Get the API key
+    const key = process.env.ELAB_KEY;
+    if (!key) {
+      console.log('Requires environment variable ELAB_KEY');
+      return;
+    }
+
+    const { flags } = await this.parse(CreateProject);
+
+    const requestBody: any = {
+      ...flags
+    };
+
+    // Handle parsing labels
+    if (requestBody.label) {
+      requestBody.label = requestBody.label.split(',');
+    }
+
+    // Make request to create the project
+    const url = 'https://sandbox.elabjournal.com/api/v1/projects';
+    const result = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Authorization': key
+      }
+    });
+
+    // Check the result
+    if (result.status == 200) {
+      const body = await result.json();
+      console.log(body);
+    } else {
+      console.log('Request failed');
+      console.log(result);
+    }
   }
 }
