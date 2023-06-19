cdaf90742cea6423675b8aa3aa6f1511b0909f3d
Rishi Shah <shahrishi108@gmail.com>
2023-06-19 12:00:32 -0700

    - added comment.module

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JobModule } from '../job/job.module';
+import { Comment, CommentSchema } from './comment.model';
+import { CommentResolver } from './comment.resolver';
+import { CommentService } from './comment.service';
 
-@Module({})
+@Module({
+    imports: [MongooseModule.forFeature([{ name: Comment.name, schema: CommentSchema }]), JobModule],
+    providers: [CommentService, CommentResolver]
+})
 export class CommentModule {}
