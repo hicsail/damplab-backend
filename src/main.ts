5ab8ef2b42e91341d10f82afb6818352844cefdf
chris-krenz <ckrenz@bu.edu>
2024-09-13 07:51:45 -0400

    reverted port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(5100);
+  await app.listen(3000);
 }
 bootstrap();
