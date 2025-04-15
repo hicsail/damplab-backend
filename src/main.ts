07e756b86999f0c8200015266b18c6cfccb5e34b
Chris Krenz <64391574+chris-krenz@users.noreply.github.com>
2025-04-15 04:25:52 -0400

    adjusted cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3100', 'http://127.0.0.1:3100'],
+    origin: [process.env.FRONTEND_URL || 'http://localhost:1100'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
