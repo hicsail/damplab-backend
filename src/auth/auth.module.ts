e86456487ecab721b587fa463b135110e8248f98
Zoe <19900057+vpsx@users.noreply.github.com>
2025-07-21 14:52:44 -0400

    Add JWT auth
    * verify tokens against keys at configured JWKs endpoint
    * toggle http or gql context

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule, JwtSecretRequestType } from '@nestjs/jwt';
+import * as jwksClient from 'jwks-rsa';
+import * as jwt from 'jsonwebtoken';
+
+/*
+This auth module extracts JWTs from request headers and verifies them
+at KEYCLOAK_JWKS_ENDPOINT. It does not issue tokens or deal with IdPs.
+*/
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const client = jwksClient({
+          jwksUri: configService.getOrThrow('KEYCLOAK_JWKS_ENDPOINT'),
+          timeout: 30000
+        });
+
+        return {
+          secretOrKeyProvider: async (requestType: JwtSecretRequestType /* unused */, tokenOrPayload: string, verifyOrSignOrOptions?: jwt.VerifyOptions | jwt.SignOptions /* unused */) => {
+            const kid = jwt.decode(tokenOrPayload, { complete: true })?.header.kid;
+            const key = await client.getSigningKey(kid);
+            const signingKey = key.getPublicKey();
+            return signingKey;
+          }
+        };
+      },
+      inject: [ConfigService]
+    })
+  ]
+})
+export class AuthModule {}
