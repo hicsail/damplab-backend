f3f79190f99822ba1346995df391949b9bfdc0c2
TheAlden <alden@jerrycarter.org>
2025-07-25 11:12:21 -0400

    Fix: announcement.service.ts

--- src/announcements/announcement.service.ts
+++ src/announcements/announcement.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Announcement, AnnouncementSchema } from './announcement.model';
+import { Announcement } from './announcement.model';
 import { CreateAnnouncementInput } from './dto/create-announcement.input';
 
 @Injectable()
