637ce79360a88e53a322c98fe3f07284f0c6b573
Kelsey Liu <kelseyliu008gmail.com>
2025-07-28 11:50:16 -0400

    refactor: update announcement feature

--- src/announcements/announcement.resolver.ts
+++ src/announcements/announcement.resolver.ts
@@ -2,6 +2,7 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { AnnouncementService } from './announcement.service';
 import { Announcement } from './announcement.model';
 import { CreateAnnouncementInput } from './dto/create-announcement.input';
+import { UpdateAnnouncementInput } from './dto/update-announcement.input';
 
 @Resolver(() => Announcement)
 export class AnnouncementResolver {
@@ -13,11 +14,18 @@ export class AnnouncementResolver {
   }
 
 
-
   @Mutation(() => Announcement)
   async createAnnouncement(
     @Args('input') input: CreateAnnouncementInput,
   ): Promise<Announcement> {
     return this.announcementService.create(input);
+  }
+
+  @Mutation(() => Announcement)
+  async updateAnnouncement(
+    @Args('timestamp', { type: () => String }) timestamp: string,
+    @Args('input') input: UpdateAnnouncementInput,
+  ): Promise<Announcement> {
+    return this.announcementService.updateByTimestamp(timestamp, input);
   }
 }
---
+++ src/announcements/dto/update-announcement.input.ts
@@ -0,0 +1,9 @@
+import { InputType, Field } from '@nestjs/graphql';
+
+@InputType()
+export class UpdateAnnouncementInput {
+
+  @Field(() => Boolean, { nullable: true })
+  is_displayed?: boolean;
+}
+
