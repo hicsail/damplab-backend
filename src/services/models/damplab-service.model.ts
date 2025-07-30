cf4d8a28257dc1804a1fde5f10bbf932ce9f84a0
TheAlden <alden@jerrycarter.org>
2025-07-30 14:51:27 -0400

    Revert changes to damplab-service.model.ts and app.module.ts

--- src/services/models/damplab-service.model.ts
+++ src/services/models/damplab-service.model.ts
@@ -48,11 +48,6 @@ export class DampLabService {
   @Prop()
   @Field()
   description: string;
-
-  @Prop()
-  @Field(() => Number, { description: 'The cost to use this service' })
-  price: number;
-
 }
 
 export type DampLabServiceDocument = DampLabService & Document;
