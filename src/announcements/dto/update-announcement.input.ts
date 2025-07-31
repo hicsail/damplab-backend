81146f6f309a1cd0c56decea16252c03737c54a5
Kelsey Liu <kelseyliu008gmail.com>
2025-07-31 15:20:56 -0400

    fix: timestamp required

--- src/announcements/dto/update-announcement.input.ts
+++ src/announcements/dto/update-announcement.input.ts
@@ -3,7 +3,7 @@ import { InputType, Field } from '@nestjs/graphql';
 @InputType()
 export class UpdateAnnouncementInput {
   @Field()
-  timestamp?: Date;
+  timestamp: Date;
 
   @Field(() => Boolean)
   is_displayed: boolean;
