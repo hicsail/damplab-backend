dc5cd6ea1e040571839aae0f8b993bcd6b5264a3
Kelsey Liu <kelseyliu008gmail.com>
2025-07-31 13:17:09 -0400

    fix: update is_displayed required

--- src/announcements/dto/update-announcement.input.ts
+++ src/announcements/dto/update-announcement.input.ts
@@ -3,7 +3,7 @@ import { InputType, Field } from '@nestjs/graphql';
 @InputType()
 export class UpdateAnnouncementInput {
 
-  @Field(() => Boolean, { nullable: true })
-  is_displayed?: boolean;
+  @Field(() => Boolean)
+  is_displayed: boolean;
 }
 
